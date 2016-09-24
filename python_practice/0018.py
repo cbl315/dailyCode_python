065dabfc749c14080e6c2aacae8c404cc198bfea
hfutcbl <583605554@qq.com>
2016-09-24 21:07:48 +0800

    python练习册_0018

--- python_practice/0018.py
+++ python_practice/0018.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# 第 0018 题： 将 第 0015 题中的 city.xls 文件中的内容写到 city.xml 文件中，如下所示：
+'''
+<?xmlversion="1.0" encoding="UTF-8"?>
+<root>
+<citys>
+<!--
+    城市信息
+-->
+{
+    "1" : "上海",
+    "2" : "北京",
+    "3" : "成都"
+}
+</citys>
+</root>
+'''
 import openpyxl
 import os
 from lxml import etree
